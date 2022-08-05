cda13b4cc1b6734003809a133cb0c62bd7a96655
Tatiana <tatiana-bondareva@inbox.ru>
2022-08-06 00:35:44 +0300

    add Task1

---
+++ Sem1/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите корень этого числа: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA == numberB*numberB)
+{
+    Console.WriteLine("Вы правы!");
+}
+else
+{
+    Console.WriteLine("Вы не правы!");
+}
+bool result = (numberA == numberB*numberB);
+Console.WriteLine(result);
