1e357fbd40e753a7780174a45daf03de7eaa6927
Tatiana <tatiana-bondareva@inbox.ru>
2022-08-10 00:47:22 +0300

    add Task13

--- Sem2/Task10/Program.cs
+++ Sem2/Task10/Program.cs
@@ -2,5 +2,15 @@
 // трёхзначное число и на выходе показывает вторую цифру этого числа
 Console.Write("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int a = ( number / 10 % 10);
+int a = number / 10 % 10;
 Console.WriteLine($"Вторая цифра введенного числа: {a}");
+
+//Второй вариант
+/*Console.Write("Введите число: ");
+string str = Console.ReadLine().ToString();
+int[] array = new int[str.Length];
+for (int i = 0; i < str.Length; i++)
+{
+    array[i] = int.Parse(str[i].ToString());
+}
+Console.WriteLine($"Вторая цифра введенного числа: {array[1]}");*/
---
+++ Sem2/Task13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет
+Console.Write("Введите число: ");
+string str = Console.ReadLine().ToString();
+int[] array = new int[str.Length];
+for (int i = 0; i < str.Length; i++)
+{
+    array[i] = int.Parse(str[i].ToString());
+}
+if (str.Length > 2)
+{
+    Console.Write($"Третья цифра введенного числа: {array[2]}");
+}
+else
+{
+    Console.Write("Неверное число");
+}
