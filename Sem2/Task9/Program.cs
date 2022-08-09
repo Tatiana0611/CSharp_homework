2bcad6ffedef324681dee22f09d59f2ef3e487c0
Tatiana <tatiana-bondareva@inbox.ru>
2022-08-09 19:25:20 +0300

    add Task9

---
+++ Sem2/Task9/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] 
+// и показывает наибольшую цифру числа.
+
+int number = new Random().Next(10, 100);
+int a = number / 10;
+int b = number % 10;
+Console.WriteLine(number);
+if (a > b) 
+    Console.Write($"Наибольшая цифра: {a}");
+else 
+    Console.Write($"Наибольшая цифра: {b}");
