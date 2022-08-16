c4996694f0d5fa59465f241267c8f9442a1c2fab
Tatiana <tatiana-bondareva@inbox.ru>
2022-08-16 23:19:56 +0300

    add Task26

---
+++ Sem4/Task26/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе
+
+// Первый вариант
+// Console.WriteLine("Введите число: ");
+// int number = Math.Abs(Convert.ToInt32(Console.ReadLine().ToString()));
+// string str = Convert.ToString(number);
+// Console.WriteLine(str.Length);
+
+// Второй вариант
+Console.WriteLine("Введите число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine().ToString()));
+int count = 0;
+while (number > 0)
+{
+    count++;
+    number /= 10;
+}
+Console.WriteLine(count);
