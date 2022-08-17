a3aa1eab8e4bc9c0c68a0e557a7deed1ba423f85
Tatiana <tatiana-bondareva@inbox.ru>
2022-08-18 00:36:05 +0300

    add Task30

---
+++ Sem4/Task30/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке
+int[] arr = new int[8];
+for (int i = 0; i < ((byte)arr.Length); i++)
+{
+    arr[i] = new Random().Next(0, 2);
+}
+Console.WriteLine('[' + string.Join(", ", arr) + ']');
