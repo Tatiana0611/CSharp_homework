8eb3d2e00c0111f5c00016863cb4b2a4fd290b12
Tatiana <tatiana-bondareva@inbox.ru>
2022-08-25 23:53:36 +0300

    add AdditionalTask

---
+++ Sem6/AdditionalTask/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// (использовать рекурсию)
+// 4 -> 24
+// 5 -> 120
+
+int Factorial(int n)
+{
+    if (n == 0 || n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{n}! = " + Factorial(n));
