920387265b739127b995b359c846fe6b5ce44267
Tatiana <tatiana-bondareva@inbox.ru>
2022-08-27 23:16:47 +0300

    add Task46

---
+++ Sem7/Task46/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 46: Задайте двумерный массив размером m x n, заполненный случайными целыми числами.
+Console.WriteLine("Задайте двумерный массив m x n");
+Console.WriteLine("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = new Random().Next(-100, 100);
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
