59e7fc1777204b3156674411f54e8dd8317e2283
Tatiana <tatiana-bondareva@inbox.ru>
2022-08-28 16:12:35 +0300

    add Task51

---
+++ Sem7/Task51/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 51:Задайте двумерный массив. Найдите сумму элементов, 
+// находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+void FillAndPrintMatrx(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 9);
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumOfMainDiagonalElements(int[,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j) sum += matrix[i, j];
+        }
+    }
+    return sum;
+}
+
+int m = new Random().Next(1, 10);
+
+int[,] matr = new int[m, m];
+FillAndPrintMatrx(matr);
+Console.WriteLine($"Сумма элементов главной диагонали: {SumOfMainDiagonalElements(matr)}");
