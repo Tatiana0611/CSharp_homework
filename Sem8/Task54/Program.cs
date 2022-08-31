54a4af0e007beae8192abacedb64da4703c4f1db
Tatiana <tatiana-bondareva@inbox.ru>
2022-08-31 16:29:49 +0300

    change Task54

--- Sem8/Task54/Program.cs
+++ Sem8/Task54/Program.cs
@@ -22,7 +22,7 @@ void FillMatrix(int[,] matrix)
 
 void PrintMatrix(int[,] matrix)
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0)-1; i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
