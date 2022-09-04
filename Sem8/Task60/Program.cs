8e3bb8a591a315cbe54a19a64793e17f7d81a852
Tatiana <tatiana-bondareva@inbox.ru>
2022-09-04 11:36:39 +0300

    add Task60

--- Sem8/Task60/Program.cs
+++ Sem8/Task60/Program.cs
@@ -6,8 +6,6 @@
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
 
-int n = 10;
-
 int[,,] array = new int[2, 2, 2];
 
 // Заполнение массива
@@ -15,19 +13,40 @@ for (int i = 0; i < array.GetLength(0); i++)
 {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-        for (int k = 0; k < array.GetLength(2); k++)
+        int k = 0;
+        while (k < array.GetLength(2))
         {
-            array[i, j, k] = n++;
+            int number = new Random().Next(10, 18);
+            if (MatrixIter(array, number)) continue;
+            array[i, j, k] = number;
+            k++;
         }
     }
 }
 
-// Печать массива
-for (int i = 0; i < array.GetLength(0); i++)
+// Проверка на повтор элементов в массиве
+bool MatrixIter(int[,,] array, int number)
 {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int k = 0; k < array.GetLength(2); k++)
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                if (array[i, j, k] == number) return true;
+            }
+        }
+    }
+    return false;
+       
+}
+
+// Печать массива
+for (int k = 0; k < array.GetLength(2); k++)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
         {
             Console.Write($"{array[i, j, k]} ({i},{j},{k})  ");
         }
